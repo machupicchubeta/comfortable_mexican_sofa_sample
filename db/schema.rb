0245c08dfa8e61dd8fb37def59f32c01e2764a32
machupicchubeta <machupicchubeta@gmail.com>
2020-01-17 16:09:53 +0900

    `bundle exec rake db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,126 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_17_070810) do
+
+  create_table "comfy_cms_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "site_id", null: false
+    t.string "label", null: false
+    t.string "categorized_type", null: false
+    t.index ["site_id", "categorized_type", "label"], name: "index_cms_categories_on_site_id_and_cat_type_and_label", unique: true
+  end
+
+  create_table "comfy_cms_categorizations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.string "categorized_type", null: false
+    t.integer "categorized_id", null: false
+    t.index ["category_id", "categorized_type", "categorized_id"], name: "index_cms_categorizations_on_cat_id_and_catd_type_and_catd_id", unique: true
+  end
+
+  create_table "comfy_cms_files", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "site_id", null: false
+    t.string "label", default: "", null: false
+    t.text "description"
+    t.integer "position", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["site_id", "position"], name: "index_comfy_cms_files_on_site_id_and_position"
+  end
+
+  create_table "comfy_cms_fragments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "record_type"
+    t.bigint "record_id"
+    t.string "identifier", null: false
+    t.string "tag", default: "text", null: false
+    t.text "content", size: :medium
+    t.boolean "boolean", default: false, null: false
+    t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["boolean"], name: "index_comfy_cms_fragments_on_boolean"
+    t.index ["datetime"], name: "index_comfy_cms_fragments_on_datetime"
+    t.index ["identifier"], name: "index_comfy_cms_fragments_on_identifier"
+    t.index ["record_type", "record_id"], name: "index_comfy_cms_fragments_on_record_type_and_record_id"
+  end
+
+  create_table "comfy_cms_layouts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "site_id", null: false
+    t.integer "parent_id"
+    t.string "app_layout"
+    t.string "label", null: false
+    t.string "identifier", null: false
+    t.text "content", size: :medium
+    t.text "css", size: :medium
+    t.text "js", size: :medium
+    t.integer "position", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["parent_id", "position"], name: "index_comfy_cms_layouts_on_parent_id_and_position"
+    t.index ["site_id", "identifier"], name: "index_comfy_cms_layouts_on_site_id_and_identifier", unique: true
+  end
+
+  create_table "comfy_cms_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "site_id", null: false
+    t.integer "layout_id"
+    t.integer "parent_id"
+    t.integer "target_page_id"
+    t.string "label", null: false
+    t.string "slug"
+    t.string "full_path", null: false
+    t.text "content_cache", size: :medium
+    t.integer "position", default: 0, null: false
+    t.integer "children_count", default: 0, null: false
+    t.boolean "is_published", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["is_published"], name: "index_comfy_cms_pages_on_is_published"
+    t.index ["parent_id", "position"], name: "index_comfy_cms_pages_on_parent_id_and_position"
+    t.index ["site_id", "full_path"], name: "index_comfy_cms_pages_on_site_id_and_full_path"
+  end
+
+  create_table "comfy_cms_revisions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.text "data", size: :medium
+    t.datetime "created_at"
+    t.index ["record_type", "record_id", "created_at"], name: "index_cms_revisions_on_rtype_and_rid_and_created_at"
+  end
+
+  create_table "comfy_cms_sites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "label", null: false
+    t.string "identifier", null: false
+    t.string "hostname", null: false
+    t.string "path"
+    t.string "locale", default: "en", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["hostname"], name: "index_comfy_cms_sites_on_hostname"
+  end
+
+  create_table "comfy_cms_snippets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.integer "site_id", null: false
+    t.string "label", null: false
+    t.string "identifier", null: false
+    t.text "content", size: :medium
+    t.integer "position", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["site_id", "identifier"], name: "index_comfy_cms_snippets_on_site_id_and_identifier", unique: true
+    t.index ["site_id", "position"], name: "index_comfy_cms_snippets_on_site_id_and_position"
+  end
+
+  create_table "comfy_cms_translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
+    t.string "locale", null: false
+    t.integer "page_id", null: false
+    t.integer "layout_id"
+    t.string "label", null: false
+    t.text "content_cache", size: :medium
+    t.boolean "is_published", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["is_published"], name: "index_comfy_cms_translations_on_is_published"
+    t.index ["locale"], name: "index_comfy_cms_translations_on_locale"
+    t.index ["page_id"], name: "index_comfy_cms_translations_on_page_id"
+  end
 
 end
